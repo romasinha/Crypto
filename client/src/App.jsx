ea12b50364e27ea521df3bfe1d172eaeecc5af83
romasinha <sinha.roma99@gmail.com>
2022-02-03 04:58:52 +0530

    basic-setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
+import {Navbar, Welcome, Footer, Services, Transactions} from './components'
 
-function App() {
+const App= () => {
   
   return (
-    <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+      <div className='gradient-bg-welcome'>
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+     
     </div>
   )
 }
---
+++ client/src/components/Transactions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transactions = () => {
+    return (
+        <div>
+            transac
+        </div>
+    )
+}
+
+export default Transactions

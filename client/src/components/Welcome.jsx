54364d6e8b3512aaf3c00a4dc07135cb70bdbfd2
romasinha <sinha.roma99@gmail.com>
2022-02-08 15:07:19 +0530

    connect wallet

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -20,12 +20,8 @@ const Input = ({placeholder,name, type, value, handleChange})=>(
 
 
 const Welcome = () => {
-    const {value} = useContext(TransactionContext);
-    console.log(value);
- 
-    const connectWallet = ()=>{
-
-    }
+    const {connectWallet} = useContext(TransactionContext);
+    
 
 const handleSubmit = ()=>{
 
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,4 +1,4 @@
-import React, {useeffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {ethers} from 'ethers'
 
 import { contractABI, contractAddress } from '../utils/constants'
@@ -20,8 +20,36 @@ const getEthereumContract = () =>{
 }
 
 export const TransactionProvider = ({children}) =>{
-  return(
-      <TransactionContext.Provider value={{value:'test'}}>
+
+  const[connectedAccount, setConnectedAccount] = useState('');
+  
+  //everytime the app reloads, useeffect called to check if wallet connected, if yes, it consoles the connected account
+  const checkIfWalletIsConnected = async ()=>{
+      if(!ethereum) return alert("Please install Metamask");
+
+      const accounts = await ethereum.request({method: 'eth_accounts'});
+      console.log(accounts);
+  }
+
+  //when we click on the button to connect wallet, this function is called which asks to connect and then sets the current account
+  const connectWallet = async ()=>{
+     try {
+        if(!ethereum) return alert("Please install Metamask"); //if not connected
+        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+        setCurrentAccount(accounts[0]); //sets the current account
+     } catch (error) {
+         console.log(error);
+
+         throw new Error("No ethereum object");
+     }
+  }
+
+  useEffect(()=>{ 
+      checkIfWalletIsConnected(); //this function is called the first time the app loads
+  }, [])
+
+  return( //connectWallet function will be passed to all components
+      <TransactionContext.Provider value={{connectWallet}}> 
           {children}
       </TransactionContext.Provider>
   )
